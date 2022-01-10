cb67ef7ea2be0663247fee0a141789061183077c
Yaasiin Asshobry <yaasiin.asshobry@pactindo.com>
2022-01-10 22:02:27 +0700

    fixing webview and button next, fixing load data json

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/services.dart';
+import 'package:pactindo_task_ui/model/eula_model.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
@@ -272,56 +273,100 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class EulaPage extends StatelessWidget {
+class EulaPage extends StatefulWidget {
   // final AssetBundle rootBundle = _initRootBundle();
+  @override
+  _EulaPageState createState() => _EulaPageState();
+}
 
-  static Future<String> loadJsonData() async {
-    var jsonText = await rootBundle.loadString('assets/data/dummy_html.json');
-    String eulaData = jsonText;
-    return eulaData;
+class _EulaPageState extends State<EulaPage> {
+  bool isChecked = false;
+
+  String contentBase64 = "";
+
+  initState() {
+    super.initState();
   }
 
-  final String contentBase64 =
-      base64Encode(const Utf8Encoder().convert(kNavigationExamplePage));
+  Future<EulaModel> loadJsonData() async { /* Jadikan return data dengan model data eula */
+    var jsonText = await rootBundle.loadString('assets/data/dummy_html.json');
+    String eulaData = jsonText;
+    EulaModel eula = EulaModel.fromJson(jsonDecode(eulaData));
+    return eula;
+  }
 
   Widget build(BuildContext context) {
     // loadJsonData();
     print('AKKKKKMMMMMMMMMMM');
     print(loadJsonData());
-    return new SingleChildScrollView(
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            child: WebView(
-              initialUrl: 'data:text/html;base64,$contentBase64',
-            ),
-          ),
-          Container(
-            width: 150,
-            height: 45,
-            child: TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.orange,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xfff58220),
+        title: Text("EULA"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder( /* Pakai future builder buat get data asynchronous */
+        future: loadJsonData(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) { /* Cek apakah datanya ada atau tidak, kalau ada tampilkan widget webview, kalau tidak tampilkan loading */
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    child: WebView(
+                      initialUrl: 'data:text/html;base64,${base64Encode(const Utf8Encoder().convert(snapshot.data.eulaHtml))}',
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Checkbox(
+                            value: isChecked,
+                            onChanged: (bool? value) { /* Set state ketika checkbox ditap/dikasih action */
+                              setState(() {
+                                isChecked = value!;
+                              });
+                            }),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            onPressed: () {
+                              if (isChecked) { /* Kasih validasi kalau checkbox belum diceklis, button gaboleh ada aksi */
+                                Navigator.pushNamed(context, '/main');
+                                setState(() {
+                                  isChecked = false;
+                                });
+                                print('I Agree Clicked');
+                              }
+                            },
+                            child: Text(
+                              "I Agree",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: Color(0xffffffff),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/main');
-                print('I Agree Clicked');
-              },
-              child: Text(
-                "I Agree",
-                style: TextStyle(
-                  fontSize: 17,
-                  color: Color(0xffffffff),
-                ),
-              ),
-            ),
-          ),
-        ],
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
